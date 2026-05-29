d072abbe8b70de2b8d0b6649ca6135d43a49e6b2
tusharchaudhari-creator <tusharchaudharucdac@gmail.com>
2026-05-29 10:22:41 +0000

    Here we go !

--- demo.js
+++ demo.js
@@ -1,2 +1,2 @@
-console.log("Tushar Chaudhari")
-console.log("Sangamner1")
+console.log("Tushar Chaudhari Git seekh chuka hai aur nahi seekhega")
+console.log("Sange Marmar waale ghar main rehta hoon")
